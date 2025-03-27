1095a1e690834cc80a679a8c6839948acd566f0d
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2025-03-27 01:58:00 -0300

    correcao unit test

--- notificationservice/src/test/java/com/carvalho/notificationservice/NotificationReceiverTest.kt
+++ notificationservice/src/test/java/com/carvalho/notificationservice/NotificationReceiverTest.kt
@@ -4,12 +4,10 @@ import android.content.Context
 import android.content.Intent
 import io.mockk.every
 import io.mockk.mockk
-import io.mockk.slot
 import io.mockk.spyk
 import io.mockk.unmockkAll
 import io.mockk.verify
 import org.junit.After
-import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
@@ -23,7 +21,11 @@ class NotificationReceiverTest : BaseTest() {
     fun setUp() {
         context = mockk(relaxed = true)
         receiver = spyk(object : NotificationReceiver() {
-            override fun createServiceIntent(context: Context, title: String?, message: String?): Intent {
+            override fun createServiceIntent(
+                context: Context,
+                title: String?,
+                message: String?
+            ): Intent {
                 val mockedIntent = mockk<Intent>(relaxed = true)
                 every { mockedIntent.getStringExtra("title") } returns title
                 every { mockedIntent.getStringExtra("message") } returns message
@@ -40,8 +42,6 @@ class NotificationReceiverTest : BaseTest() {
     @Test
     fun `onReceive deve iniciar o NotificationService com extras corretos`() {
         val incomingIntent = mockk<Intent>(relaxed = true)
-        every { incomingIntent.getStringExtra("title") } returns "Título Teste"
-        every { incomingIntent.getStringExtra("message") } returns "Mensagem Teste"
 
         every { context.startService(any()) } returns null
 
